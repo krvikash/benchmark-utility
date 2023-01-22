774bfa7de1d2f2e571a80464a5b64b2fc013043e
Vikash Kumar <vikash.kumar@starburstdata.com>
2023-01-23 00:59:04 +0530

    Remove unused import

--- src/main/java/io/krvikash/iceberg/benchmark/S3Client.java
+++ src/main/java/io/krvikash/iceberg/benchmark/S3Client.java
@@ -19,12 +19,8 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ListObjectsV2Request;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.google.common.collect.ImmutableList;
 import io.krvikash.iceberg.benchmark.statistics.NDV;
 
-import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static io.krvikash.iceberg.benchmark.BenchmarkUtils.getDataPath;
